3b240301b36b1d9718626f3ef4c7ae58d8f87aaa
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-10-02 14:57:21 +0300

    Searchbar on hoocks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,45 @@
-import { Component } from "react";
+import { useState } from "react";
 import {GoSearch} from 'react-icons/go'
 import styles from "../Searchbar/Searchbar.module.css"
 import PropTypes from 'prop-types';
 
-export default class Searchbar extends Component{
-    
-    state = {
-        searchQuerry: ""
-    }
+const Searchbar = ({onSubmit}) => {
+    const [searchQuerry, setSearchQuerry] = useState('');
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const { searchQuerry } = this.state;
-        if (searchQuerry.trim() === '') {
-            alert('Please enter something to searh querry');
-            return;
-        }
-        this.props.onSubmit(searchQuerry);
+        (searchQuerry.trim() === '')?
+            alert('Please enter something to searh querry'):
+            onSubmit(searchQuerry);
     }
     
-    handleInput = (event) => {
-        this.setState({ searchQuerry: event.currentTarget.value.toLowerCase() });
+    const handleInput = (event) => {
+        setSearchQuerry(event.target.value.toLowerCase());
     }
 
-    render() {
-        const { searchQuerry } = this.state;
-        const { handleInput, handleSubmit } = this;
-        return(
-            <header className="searchbar">
-              <form className={styles.searchForm} onSubmit = {handleSubmit}>
+    return (
+        <header className="searchbar">
+            <form className={styles.searchForm}
+                onSubmit={handleSubmit}>
                 <button type="submit" className={styles.button}>
-                        <GoSearch className={styles.searchIcon}/>
+                    <GoSearch className={styles.searchIcon}/>
                 </button>
 
                 <input
-                    className={styles.input}
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    name = "searchQuerry"
-                    value = {searchQuerry}
-                    onInput={handleInput}
+                className={styles.input}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                name = "searchQuerry"
+                value = {searchQuerry}
+                onInput={handleInput}
                 />
-              </form>
-            </header>
-        )
-    }
+            </form>
+        </header>
+    )
 }
+export default Searchbar;
 
 Searchbar.propTypes = {
     onSubmit: PropTypes.func.isRequired
